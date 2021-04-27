f67a72400c17f1a97978d85f69b3d6582e0afce1
ShihChu <juju0411tw@gmail.com>
2021-04-27 10:58:37 +0800

    Create migrations and rb files

---
+++ app/db/migrations/001_restaurants_create.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:restaurants) do
+      primary_key :id
+
+      String :name, unique: true, null: false
+      String :website, unique: true
+      String :address, unique: true
+      String :menu
+
+      DataTime :created_at
+      DataTime :updated_at
+    end
+  end
+end
---
+++ config/environments.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'roda'
+require 'figaro'
+require 'sequel'
+
+module RestaurantCollections
+  # Configuration for the API
+  class Api < Roda
+    plugin :environments
+
+    Figaro.application = Figaro::Application.new(
+      environment: environment,
+      path: File.expand_path('config/secrets.yml')
+    )
+    Figaro.load
+
+    # Make the enviroment variables accessible to other classes
+    def self.config
+      Figaro.env
+    end
+
+    # Connect to the right DB
+    DB = Sequel.connect(config.DATABASE_URL)
+
+    # Make the database accessible to other classes
+    def self.db
+      DB
+    end
+
+    # Development console (like irb) for dev and test ENV
+    configure :development, :test do
+      require 'pry'
+    end
+  end
+end
