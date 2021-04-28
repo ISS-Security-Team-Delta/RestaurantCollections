0617f30a6e82c3a68f99587c1ee4cd944814c9dc
tara-thomas <t.tarathomas@gmail.com>
2021-04-28 21:55:36 +0800

    Commiting before merge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,57 +5,55 @@ require 'base64'
 require 'rbnacl'
 
 module RestaurantCollections
-    STORE_DIR = 'app/db/store'
+  STORE_DIR = 'app/db/store'
 
-    class Restaurant
-        def initialize(new_restaurant)
-            @id      = new_restaurant['id'] || new_id
-            @website = new_restaurant['website']
-            @name    = new_restaurant['name']
-            @address = new_restaurant['address']
-            @menu    = new_restaurant['menu'] # this will be a string for now, will be image later :D
-        end
-
-        attr_reader :id, :website, :name, :address, :menu
-
-        def to_json(options = {})
-            JSON(
-                {
-                    type: 'restaurant',
-                    id: id,
-                    website: website,
-                    name: name,
-                    address: address,
-                    menu: menu
-                },
-                options
-            )
-        end 
-
-        def self.setup
-            Dir.mkdir(RestaurantCollections::STORE_DIR) unless Dir.exist? RestaurantCollections::STORE_DIR
-        end
-
-        def save
-            File.write("#{RestaurantCollections::STORE_DIR}/#{id}.txt", to_json)
-        end
-
-        def self.find(find_id)
-            restaurant_data = File.read("#{RestaurantCollections::STORE_DIR}/#{find_id}.txt") 
-            Restaurant.new JSON.parse(restaurant_data)
-        end
-
-        def self.all
-            Dir.glob("#{RestaurantCollections::STORE_DIR}/*.txt").map do |file| 
-                file.match(%r{#{Regexp.quote(RestaurantCollections::STORE_DIR)}\/(.*)\.txt})[1]
-            end
-        end
-
-        private
-
-        def new_id
-            timestamp = Time.now.to_f.to_s
-            Base64.urlsafe_encode64(RbNaCl::Hash.sha256(timestamp))[0..9]
-        end
+  class Restaurant
+    def initialize(new_restaurant)
+      @id      = new_restaurant['id'] || new_id
+      @website = new_restaurant['website']
+      @name    = new_restaurant['name']
+      @address = new_restaurant['address']
+      @menu    = new_restaurant['menu'] # this will be a string for now, will be image later :D
     end
+    attr_reader :id, :website, :name, :address, :menu
+
+    def to_json(options = {})
+      JSON(
+        {
+          type: 'restaurant',
+          id: id,
+          website: website,
+          name: name,
+          address: address,
+          menu: menu
+        }, options
+      )
+    end
+
+    def self.setup
+      Dir.mkdir(RestaurantCollections::STORE_DIR) unless Dir.exist? RestaurantCollections::STORE_DIR
+    end
+
+    def save
+      File.write("#{RestaurantCollections::STORE_DIR}/#{id}.txt", to_json)
+    end
+
+    def self.find(find_id)
+      restaurant_data = File.read("#{RestaurantCollections::STORE_DIR}/#{find_id}.txt") 
+      Restaurant.new JSON.parse(restaurant_data)
+    end
+
+    def self.all
+      Dir.glob("#{RestaurantCollections::STORE_DIR}/*.txt").map do |file|
+        file.match(%r{#{Regexp.quote(RestaurantCollections::STORE_DIR)}\/(.*)\.txt})[1]
+      end
+    end
+
+    private
+
+    def new_id
+      timestamp = Time.now.to_f.to_s
+      Base64.urlsafe_encode64(RbNaCl::Hash.sha256(timestamp))[0..9]
+    end
+  end
 end
