619a1957aa887b26720ecee340b109f5583b4bc8
Steventime <32660539+Steventime0430@users.noreply.github.com>
2021-04-29 21:41:13 +0800

    Api Spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,14 @@
 ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'yaml'
+
+require_relative 'test_load_all'
+
+def wipe_database
+  app.DB[:restaurants].delete
+end
+
+DATA = {}
+DATA[:restaurants] = YAML.safe_load File.read('app/db/seeds/restaurant_seeds.yml')
