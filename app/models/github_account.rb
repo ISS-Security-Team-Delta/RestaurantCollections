23b6a4f039abcb7448ddddc069e5965a60bb6313
tara-thomas <t.tarathomas@gmail.com>
2021-06-17 20:38:18 +0800

    Added OAuth Code: Needs to Fix Errors

---
+++ app/models/github_account.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module RestaurantCollections
+    # Maps Github account details to attributes
+    class GithubAccount
+      def initialize(gh_account)
+        @gh_account = gh_account
+      end
+      def username
+        @gh_account['login'] + '@github'
+      end
+
+      def email
+        @gh_account['email']
+      end
+    end
+end
